738eff5905d8b47d8ae6e28b1f8cd3f867f67f3a
Shane Paulus <shanepaulus@gmail.com>
2021-08-13 19:44:22 +0200

    Changed project structure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const routes = require('./api/routes/orderListRoutes');
+const routes = require('./src/routes/orderListRoutes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
--- e2e/api/controller/orderController.test.js
+++ e2e/api/controller/orderController.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-describe('Simple math test', () => {
+describe('Simple math e2e', () => {
 	it('should return 2', () => {
 		assert.equal(1 + 1, 2);
 	})
